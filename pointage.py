c113faadb033ef63650b6722b9843b59b6349595
Dadowatt <dadowatt92@gmail.com>
2026-02-06 15:22:03 +0100

    ajout des fonctionnalité de recherche et affichage des apprenants

--- pointage.py
+++ pointage.py
@@ -23,4 +23,27 @@ def ajouter_apprenant():
     print('Apprenant ajouté avec succès')
 
 ajouter_apprenant()
-    
+
+def afficher_apprenants():
+    curseur.execute("SELECT * FROM apprenants WHERE presence = 'present'")
+    resultats = curseur.fetchall()
+    if resultats:
+        print('\Liste des apprenants pointés')
+        for apprenants in resultats:
+            print(apprenants)
+afficher_apprenants()
+
+def rechercher_apprenant():
+    recherche = input("Saisir l'ID de l'apprenant: ")
+    if not recherche.isdigit():
+        print("L'ID doit être un nombre")
+        return
+    curseur.execute("SELECT * FROM apprenants WHERE id = %s", (int(recherche),))
+    resultats = curseur.fetchall()
+    if resultats:
+        print("Aprenant trouvé: ")
+        for apprenant in resultats:
+            print(apprenant)
+rechercher_apprenant()
+curseur.close()
+connexion.close()
